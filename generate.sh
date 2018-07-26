9f67face282926ff41e8036a8bb347ea8ae0989e
tianser <cp542524698@gmail.com>
2018-07-26 18:56:17 +0800

    add post
    Signed-off-by: tianser <cp542524698@gmail.com>

--- generate.sh
+++ generate.sh
@@ -2,6 +2,6 @@ git checkout dev -- public
 ls public/ |xargs -n 1 -i cp public/{} ./ -rf
 rm public -rf
 
-#git add --all .
-#git commit -s -m "add post"
-#git push
+git add --all .
+git commit -s -m "add post"
+git push
